0c873b41108530170cbd9ae226f870e54b5bb4a3
fabio-mayer <43644909+fabio-mayer@users.noreply.github.com>
2018-10-01 16:17:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="js/chatbot.js"></script>
-    <link rel="stylesheet" href="css/chatbot.css" type="text/css">
+    <script src="chatbot.js"></script>
+    <link rel="stylesheet" href="chatbot.css" type="text/css">
     
 </head>
 <body>
